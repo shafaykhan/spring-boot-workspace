3b46a40d4f0b48620afb74ad23c9fc5b1d7b7ad3
shafayk <user7@sunrichtech.com>
2023-04-01 14:59:00 +0530

    Initial commit
    Author- KhanShafay

---
+++ src/main/java/com/shafay/springbootworkspace/SpringBootWorkspaceApplication.java
@@ -0,0 +1,13 @@
+package com.shafay.springbootworkspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWorkspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWorkspaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shafay/springbootworkspace/SpringBootWorkspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shafay.springbootworkspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWorkspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
